d42e9fbfa80eaf1f223555fff11e90139197cf31
Alexander Haase <alex@sensorstar.com>
2015-04-28 17:05:29 -0400

    Abstract MenuActivity created to centralize menu creation.

--- src/android/app/src/main/java/com/movo/wave/DiscoverWaveActivity.java
+++ src/android/app/src/main/java/com/movo/wave/DiscoverWaveActivity.java
@@ -24,21 +24,15 @@ import java.util.Calendar;
  * Created by Alex Haase on 3/23/2015.
  */
 
-public class DiscoverWaveActivity extends ActionBarActivity {
-    Context c;
+public class DiscoverWaveActivity extends MenuActivity {
     ArrayList<String> waves;
     ListView waveList;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
-    String[] menuOptions;
 
     public String TAG = "Movo Discover Wave";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discover_wave);
-        c = this.getApplicationContext();
+        initMenu(R.layout.activity_discover_wave);
         waveList = (ListView) findViewById(R.id.waveList);
 
         waves = new ArrayList<String>();
@@ -60,90 +54,5 @@ public class DiscoverWaveActivity extends ActionBarActivity {
                 finish();
             }
         });
-
-
-//**********************Set Up slider menu******************//
-        menuOptions = new String[6];
-        menuOptions[0] = "Login";
-        menuOptions[1] = "Upload Data";
-        menuOptions[2] = "waves";
-        menuOptions[3] = "FAQ";
-        menuOptions[4] = "Contact Us";
-        menuOptions[5] = "Logout";
-
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList.setBackgroundResource(R.drawable.splash);
-
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(DiscoverWaveActivity.this,
-                R.layout.drawer_list_item, menuOptions));
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-
-        Toolbar mToolbar = (Toolbar)findViewById(R.id.toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,  mDrawerLayout, mToolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        );
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        mDrawerToggle.syncState();
-        //*********************************************************//
-
-
     }
-
-    /**
-     * Swaps fragments in the main content view
-     */
-    private void selectItem(int position) {
-
-
-        mDrawerList.setItemChecked(position, false);
-        mDrawerLayout.closeDrawer(mDrawerList);
-        switch (position)
-        {
-            case 0:
-//                create();
-                Log.d(TAG, "Login pressed");
-//                login();
-                break;
-            case 1:
-                Log.d(TAG, "Upload pressed");
-                UserData myData = UserData.getUserData(c);
-                Log.d(TAG, "Cur user data: "+myData.getCurUID());
-
-//                login();
-                break;
-            case 2:
-                Log.d(TAG, "waves pressed");
-//                waves();
-
-            case 3:
-                Log.d(TAG, "FAQ pressed");
-//                logout();
-                break;
-            case 4:
-                Log.d(TAG, "Contact pressed");
-//                match();
-                break;
-
-            case 5:
-                Log.d(TAG, "Logout pressed");
-//                logout();
-                break;
-        }
-    }
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
-
 }
---
+++ src/android/app/src/main/java/com/movo/wave/MenuActivity.java
@@ -0,0 +1,138 @@
+package com.movo.wave;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.movo.wave.util.LazyLogger;
+
+/**
+ * Created by alex on 4/28/2015.
+ */
+public abstract class MenuActivity extends ActionBarActivity {
+    final static LazyLogger lazyLog = new LazyLogger( "MenuActivity");
+
+    public static enum Option {
+        LifeCycle   ("My Life Calendar", Home.class ),
+        MyProfile   ("My Profile", com.movo.wave.MyProfile.class ),
+        UploadData  ("Upload Data", Home.class),
+        User        ("Users", UserActivity.class ),
+        FAQ         ("FAQ", null),
+        Contact     ("Contact", null),
+        Logout      ("Logout", null) {
+            @Override
+            public Intent select(Context context) {
+                UserData mUD = UserData.getUserData(context);
+                return new Intent( context, mUD.logoutCurrentUser() ? FirstLaunch.class : Home.class );
+            }
+        },
+        DiscoverWave    ( "Discover Wave", DiscoverWaveActivity.class),
+        ;
+
+        final public String text;
+        final public Class<?> activity;
+
+        private Option( String text, Class<?> activity ) {
+            this.text = text;
+            this.activity = activity;
+        }
+
+        public Intent select( Context context ) {
+            Intent ret = null;
+            if( activity != null ) {
+                ret = new Intent( context, activity );
+            } else {
+                lazyLog.e( "No activity class for menu option ", name() );
+            }
+            return ret;
+        }
+
+        public static final String[] names;
+
+        static {
+            names = new String[ Option.values().length ];
+            for( Option option : Option.values() ) {
+                names[ option.ordinal() ] = option.text;
+            }
+        }
+    }
+
+    protected DrawerLayout mDrawerLayout;
+    protected ListView mDrawerList;
+    protected ActionBarDrawerToggle mDrawerToggle;
+    protected Context c;
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            mDrawerList.setItemChecked(position, false);
+            mDrawerLayout.closeDrawer(mDrawerList);
+
+            if( position >= Option.values().length ) {
+                lazyLog.e( "Menu ordinal out of range: " + position );
+            } else {
+                final Intent intent = Option.values()[position].select(c);
+                if (intent != null) {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }
+    }
+
+    protected void initMenu( int layout_id ) {
+        c = getApplicationContext();
+        setContentView(layout_id);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList.setBackgroundResource(R.drawable.splash);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, Option.names ));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+
+        Toolbar mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,  mDrawerLayout, mToolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
