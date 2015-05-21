4657e8d614ded87eea1b33c920cca6ead65e4916
Rudy Yukich <rudy@sensorstar.com>
2015-05-21 14:51:43 -0400

    tweaks to navigation menu on daily screen.
    beginnings of usermenubar changes to support stats page switching

---
+++ src/iOS_Client/Wave/Wave/UserMenuBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserMenuBarViewController.swift
+//  Wave
+//
+//  Created by Rudy Yukich on 5/21/15.
+//
+//
+
+import Foundation
+import UIKit
+
+class UserMenuBarViewController : UIViewController {
+    
+    @IBAction func statsButtonPress(sender: AnyObject) {
+        
+        
+
+    }
+    
+}
