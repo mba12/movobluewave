39b4c893c6ec4f2151097586d010c5b41c2d3469
Rudy Yukich <rudy@sensorstar.com>
2015-06-09 10:42:44 -0400

    Added image source selection logic... detects sources and displays popover dialog based on available sources

---
+++ src/iOS_Client/Wave/Wave/imageSourceSelection.swift
@@ -0,0 +1,56 @@
+//
+//  imageSourceSelection.swift
+//  Wave
+//
+//  Created by Rudy Yukich on 6/9/15.
+//
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+
+protocol ImageSourceSelectionDelegate {
+    func didSelectSource(useCamera: Bool)
+}
+
+
+class ImageSourceSelection {
+    static func pickImageSource(parent: UIViewController, delegate: ImageSourceSelectionDelegate, location : CGRect?) {
+        var didAddAction = false
+        let alert : UIAlertController = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let takePicture = UIAlertAction(title: "Take photo", style: UIAlertActionStyle.Default, handler: {action in delegate.didSelectSource(true) })
+        
+        let pickPhoto = UIAlertAction(title: "Choose from library", style: UIAlertActionStyle.Default, handler: {
+            action in delegate.didSelectSource(false) }
+        )
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)) {
+            alert.addAction(takePicture)
+            didAddAction = true
+        }
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum)) {
+            alert.addAction(pickPhoto)
+            didAddAction = true
+        }
+        
+        alert.modalInPopover = false
+        
+        if let presenter = alert.popoverPresentationController {
+            presenter.sourceView = parent.view
+            if let loc = location {
+                presenter.sourceRect = loc
+            } else {
+                presenter.sourceRect = CGRectMake(parent.view.bounds.size.width-20, 80.0, 1.0, 1.0)
+            }
+            
+            presenter.permittedArrowDirections = UIPopoverArrowDirection.Up
+            
+        }
+        if (didAddAction) {
+            parent.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+    }
+}
