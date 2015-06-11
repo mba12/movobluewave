5adccd9f5eec9fc9b93a244a9b6a2ce12816212d
Rudy Yukich <rudy@sensorstar.com>
2015-06-11 12:05:11 -0400

    -> Custom margins for iPhone6 / iPhone6P

--- src/iOS_Client/Wave/Wave/rtlFlowLayout.swift
+++ src/iOS_Client/Wave/Wave/rtlFlowLayout.swift
@@ -14,11 +14,43 @@ class rtlFlowLayout : UICollectionViewFlowLayout {
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.itemSize = CGSize(width: 95, height: 95)
+        //handle iPhone6 and iPhone6P//
+        var isPhone = false
+        var isIPhone6 = false
+        var isIPhone6P = false
+        if (UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Phone) {
+            isPhone = true
+            if (UIScreen.mainScreen().bounds.size.height == 667.0) {
+                isIPhone6 = true
+            }
+            if (UIScreen.mainScreen().scale > 2.9) {
+                isIPhone6 = true
+                isIPhone6P = true
+            }
+            if (UIScreen.mainScreen().nativeScale > 2.9) {
+                isIPhone6 = true
+                isIPhone6P = true
+            }
+            if (UIScreen.mainScreen().bounds.size.height == 736.0) {
+                isIPhone6 = true
+                isIPhone6P = true
+            }
+                
+        }
+        if (!isIPhone6) {
+            self.itemSize = CGSize(width: 95, height: 95)
+        } else {
+            if (!isIPhone6P) {
+                self.itemSize = CGSize(width: 110, height: 110)
+            } else {
+                self.itemSize = CGSize(width: 120, height: 120)
+            }
+        }
         self.minimumInteritemSpacing = 7.5
         self.minimumLineSpacing = 5
         
     }
+    
     override func layoutAttributesForElementsInRect(rect: CGRect) -> [AnyObject]? {
         var supersAttributes = super.layoutAttributesForElementsInRect(rect)
         
